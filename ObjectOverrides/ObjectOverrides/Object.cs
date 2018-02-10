2e7d89c8200cc1d3301e67a5bf97e6053045f189
Your Name <your_email@whatever.com>
2018-02-10 17:36:55 +0700

    laba4(2)
    ochen slojna

---
+++ ObjectOverrides/ObjectOverrides/Object.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    public class Object
+    {
+        /* Виртуальные чены.
+        public virtual bool Equals(object obj);
+        protected virtual void Finalize();
+        public virtual int GetHashCode();
+        public virtual string ToString();
+
+        // Невиртуальные члены уровня экземпляра.
+        public Type GetType();
+        protected object MemberwiseClone();
+
+        // Статические члены.
+        public static bool Equals(object objA, object objB);
+        public static bool ReferenceEquals(object objA, object objB);*/
+
+        static void StaticMembersOfObject()
+        {
+            // Статические члены System.Object.
+            Person p3 = new Person("Sally", "Jones", 4);
+            Person p4 = new Person("Sally", "Jones", 4);
+            Console.WriteLine("P3 and P4 have same state: {0}", object.Equals(p3, p4));
+            Console.WriteLine("P3 and P4 are pointing to same object: {0}", object.ReferenceEquals(p3, p4));
+        }
+    }
+}
---
+++ ObjectOverrides/ObjectOverrides/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with System.Object *****\n");
+            Person p1 = new Person();
+
+            // Использовать унаследованне члены System.Object.
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+
+            // Создать другую ссылку на p1.
+            Person p2 = p1;
+            object o = p2;
+
+            // Указывают ли ссылки на один и то же объект в памяти?
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!"); // Один и тот же экземпляр.
+            }
+            Console.ReadLine();
+        }
+    }
+}
